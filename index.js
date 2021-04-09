6371cf14a246f79a9542094371b2297624f772bd
José Lindemberg <44483365+berg1618@users.noreply.github.com>
2021-04-09 20:25:09 -0300

    Create initial setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+//view engine
+app.set('view engine','ejs');
+
+app.use(node.urlencoded({extends: false}));
+app.use(node.json());
+
+app.get('/', (req,res) => {
+	res.render('index');
+});
+
+app.listen(8080, () =>{
+	console.log('app working!');
+})
