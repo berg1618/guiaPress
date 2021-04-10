a78a3c665edc2cf61fd5b7845d1438a7ef92a012
José Lindemberg <44483365+berg1618@users.noreply.github.com>
2021-04-10 08:35:28 -0300

    Install Bootstrap and, finish setup

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
+const connection = require('./database/database');
 
 //view engine
 app.set('view engine','ejs');
 
-app.use(node.urlencoded({extends: false}));
-app.use(node.json());
+//Static
+app.use(express.static('public'));
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//database
+connection.authenticate()
+	.then(() => {
+		console.log('successful connection');
+	}).catch((error) => {
+		console.log(error);
+	})
 
 app.get('/', (req,res) => {
 	res.render('index');
