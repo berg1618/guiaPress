3a081039cb70e33280ef8cbb0c0d28dbb918e18a
José Lindemberg <44483365+berg1618@users.noreply.github.com>
2021-04-14 15:16:48 -0300

    Create pages to add and display articles

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,57 @@
 const express = require('express');
 const router = express.Router();
+const Category = require('../categories/Category');
+const Article = require('./Article');
+const slugify = require('slugify');
 
-router.get('/articles',(req, res) => {
-	res.send('rota de articles');
+router.get('/admin/articles',(req, res) => {
+	Article.findAll({
+		include: [{model: Category, required: true}]
+	}).then(articles => {
+		res.render('admin/articles/index', {articles: articles});	
+	})
 });
 
 router.get('/admin/articles/new',(req, res) => {
-	res.send('router to create a new article');
+	Category.findAll().then(categories => {
+		res.render('admin/articles/new',{categories: categories});
+	})
 });
+
+router.post('/articles/save', (req, res) => {
+	const title = req.body.title;
+	const body = req.body.body;
+	const category = req.body.category;
+
+	Article.create({
+		title: title,
+		slug: slugify(title),
+		body: body,
+		categoryId: category
+	}).then(() => {
+		res.redirect('/admin/articles')
+	});
+});
+
+router.post('/articles/delete', (req, res) => {
+	const id = req.body.id;
+	if(id != undefined){
+		if(!isNaN(id)){
+
+			Article.destroy({
+				where: {
+					id: id
+				}
+			}).then(() => {
+				res.redirect('/admin/articles');
+			})
+			
+		}else{//not a number
+			res.redirect('/admin/articles');
+		}
+	}else{//Null
+		res.redirect('/admin/articles');
+	}
+})
 
 module.exports = router;
