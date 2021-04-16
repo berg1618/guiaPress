e93b9b6e09a59008eb9f3c2c4b1829ede5ac94e1
José Lindemberg <44483365+berg1618@users.noreply.github.com>
2021-04-16 09:50:08 -0300

    Create paging system

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -74,4 +74,64 @@ router.get('/admin/articles/edit/:id', (req, res) => {
 	})
 });
 
+router.post('/articles/update', (req, res) => {
+	const id = req.body.id;
+	const title = req.body.title;
+	const body = req.body.body;
+	const category = req.body.category;
+
+	Article.update({
+		title: title,
+		body: body,
+		categoryId: category,
+		slug: slugify(title)
+	},{
+		where: {
+			id: id
+		}
+	}).then(() => {
+		res.redirect('/admin/articles');
+	}).catch((err) => {
+		res.redirect('/');
+	})
+})
+
+router.get('/articles/page/:num', (req, res) => {
+	const page = req.params.num;
+	let offset = 0;
+
+	if(isNaN(page)|| page == 1){
+		offset = 0;
+	}else{
+		offset = parseInt(page) * 4;
+	}
+
+	Article.findAndCountAll({
+		limit: 4,
+		offset: offset,
+		order:[
+			['id', 'DESC']
+		],
+	}).then(articles => {
+
+		let next;
+
+		if(offset + 4 >= articles.count){
+			next = false;
+		} else {
+			next = true;
+		}
+
+		const result = {
+			next: next,
+			articles: articles
+		}
+
+		Category.findAll().then(categories => {
+			res.render('admin/articles/page', {result: result, categories: categories})
+		})
+
+	})
+})
+
 module.exports = router;
