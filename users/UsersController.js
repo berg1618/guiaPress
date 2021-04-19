fac691c49ab871ee29539def0bc7c4664ee19e4a
José Lindemberg <44483365+berg1618@users.noreply.github.com>
2021-04-19 09:27:47 -0300

    Install Bcryptjs

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('./User');
+const bcrypt = require('bcryptjs');
 
 router.get('/admin/users', (req, res) => {
 	res.send('hello user');
@@ -14,7 +15,17 @@ router.post('/users/create', (req, res) => {
 	const email = req.body.email;
 	const password = req.body.password; 
 
-	res.json({email, password});
+	const salt = bcrypt.genSaltSync(10);
+	const hash = bcrypt.hashSync(password);
+
+	User.create({
+		email: email,
+		password: hash
+	}).then(() => {
+		res.redirect('/');
+	}).catch( (err) => {
+		res.redirect('/');
+	})
 });
 
 module.exports = router;
