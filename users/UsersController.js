6d2ec0b3f4d3f660083dee1ab7b2bd3dd93ad301
José Lindemberg <44483365+berg1618@users.noreply.github.com>
2021-04-19 15:22:16 -0300

    Create session system

--- users/UsersController.js
+++ users/UsersController.js
@@ -4,7 +4,9 @@ const User = require('./User');
 const bcrypt = require('bcryptjs');
 
 router.get('/admin/users', (req, res) => {
-	res.send('hello user');
+	User.findAll().then(users => {
+		res.render('admin/users/index', {users: users});
+	});
 });
 
 router.get('/admin/users/create', (req, res) => {
@@ -15,17 +17,58 @@ router.post('/users/create', (req, res) => {
 	const email = req.body.email;
 	const password = req.body.password; 
 
-	const salt = bcrypt.genSaltSync(10);
-	const hash = bcrypt.hashSync(password);
+	User.findOne({
+		where: {email: email}
+	}).then(user => {
+		if(user == undefined){
 
-	User.create({
-		email: email,
-		password: hash
-	}).then(() => {
-		res.redirect('/');
-	}).catch( (err) => {
-		res.redirect('/');
+			const salt = bcrypt.genSaltSync(10);
+			const hash = bcrypt.hashSync(password);
+
+			User.create({
+				email: email,
+				password: hash
+			}).then(() => {
+				res.redirect('/');
+			}).catch( (err) => {
+				res.redirect('/');
+			})
+
+		}else{
+			res.redirect('/admin/users/create');
+		}
 	})
+
+	
 });
+
+router.get('/login', (req, res) => {
+	res.render('admin/users/login');
+});
+
+router.post('/authenticate', (req, res) => {
+	const email = req.body.email;
+	const password = req.body.password;
+
+	User.findOne({where:{email:email}}).then(user => {
+		if(user != undefined){//if there's a user with this email
+			//password validation
+			const correct = bcrypt.compareSync(password, user.password);
+
+			if(correct){
+				req.session.user = {
+					id: user.id,
+					email: user.email
+				}
+				res.json(req.session.user);
+			}else{
+				res.redirect('/login');
+			}
+
+		}else{
+			res.redirect('/login');
+		}
+	})
+})
 
 module.exports = router;
